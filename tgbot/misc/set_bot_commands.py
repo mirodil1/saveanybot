6c7811c56248c870eb378e6509ca1fa88e27f416
mirodil <ortikov.m97@gmail.com>
2022-10-11 12:29:13 +0500

    new translations

--- tgbot/misc/set_bot_commands.py
+++ tgbot/misc/set_bot_commands.py
@@ -1,11 +1,12 @@
 from aiogram import types
+from tgbot.middlewares.i18n import _
 
 async def set_default_commands(bot):
         await bot.set_my_commands(
-        
+
             [
-                types.BotCommand("start", "Restart bot"),
-                types.BotCommand("lang", "Change language"),
+                types.BotCommand("start", _("Restart bot")),
+                types.BotCommand("lang", _("Change language")),
 
             ]
         )
