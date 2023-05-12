b3dafa7714222a56a715c7d4a1b267542c3d7444
mirodil <ortikov.m97@gmail.com>
2023-05-12 16:56:24 +0500

    message for all users

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,16 +1,21 @@
+import re
+import asyncio
+
 from aiogram import Dispatcher, Bot
 from aiogram import types
 from aiogram.dispatcher import filters, FSMContext
 from aiogram.utils.deep_linking import get_start_link
+
 from tgbot.keyboards.inline import  response_callback, send_msg_button, response_callback_confirm
 from tgbot.misc.states import Message, Link
 from tgbot.services.db import db
-import asyncio
-
 from tgbot.config import load_config
+
 
 config = load_config(".env")
 bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+
+
 async def admin_start(message: Message):
     await message.reply("Hello, admin!")
 
@@ -18,7 +23,8 @@ async def admin_start(message: Message):
 async def command_link(message: Message):
     await message.answer("Link uchun so'zni kiriting.\nA-Z, a-z, 0-9, _ va - ruxsat etiladi.")
     await Link.link.set()
-    
+
+
 async def get_deep_link(message: types.Message, state: FSMContext):
     link = await get_start_link(message.text)
     await message.answer(link)
@@ -26,16 +32,18 @@ async def get_deep_link(message: types.Message, state: FSMContext):
 
 
 async def ads(message: types.Message):
-        await message.answer("Istagan xabaringizni kiriting")
-        await Message.message.set()
+    await message.answer("Istagan xabaringizni kiriting")
+    await Message.message.set()
+
 
 async def prepare_to_send(message: types.Message, state: FSMContext):
-    counter = await db.count_users_by_language()
+    counter = await db.count_users()
     await bot.copy_message(from_chat_id=message.from_user.id, chat_id=message.from_user.id, message_id=message.message_id)
     await message.answer(f"Xabar {counter} foydalanuchiga yuboriladi", reply_markup=send_msg_button)
     async with state.proxy() as data:
         data['message'] = message.message_id
         data['from_user_id'] = message.from_user.id
+
 
 async def confirmation(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
     call_data = callback_data['confirm']
@@ -44,7 +52,7 @@ async def confirmation(call: types.CallbackQuery, state: FSMContext, callback_da
         data = await state.get_data()
         from_user_id = data.get('from_user_id')
         message_id = data.get('message')
-        users = await db.select_all_uz_users()
+        users = await db.select_all_users()
         await call.message.edit_text("Xabar yuborilyapti")
         await state.finish()
         for user in users:
